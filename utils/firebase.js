d34912764398e7561f107800c3faca2fccab2bc6
Dextrothrift <dextro.thrift@gmail.com>
2025-04-26 14:06:23 +0530

    akonfire

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,6 +1,8 @@
 // utils/firebase.js
 const admin = require('firebase-admin');
-const serviceAccount = require('../firebase-adminsdk.json');
+
+// Parse the JSON blob from the environment
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
